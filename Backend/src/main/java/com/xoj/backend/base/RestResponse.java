6e48abf31171c5c6620a300a7ad453a898b6fd04
cjh <2637212633@qq.com>
2022-03-12 17:27:36 +0800

    feature: Add RestResponse RestRequest

---
+++ Backend/src/main/java/com/xoj/backend/base/RestResponse.java
@@ -0,0 +1,45 @@
+package com.xoj.backend.base;
+
+import lombok.*;
+
+/***
+ * @Author yezhen
+ * @Date 16:37 2022/3/12
+ ***/
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestResponse<T> {
+    private T obj;
+    private String comment;
+    private int status;
+
+    public static <T> RestResponse<T> ok(T obj){
+        return new RestResponse<T> (obj,null,1);
+    }
+
+    public static <T> RestResponse<T>  ok(T obj, String comment){
+        return new RestResponse<T> (obj,null,1);
+    }
+
+    public static <T> RestResponse<T>  ok(){
+        return new RestResponse<T> (null,null,1);
+    }
+
+    public static <T> RestResponse<T>  error(T obj){
+        return new RestResponse<T> (obj,null,-1);
+    }
+
+    public static <T> RestResponse<T>  error(T obj, String comment){
+        return new RestResponse<T> (obj,null,-1);
+    }
+
+    public static <T> RestResponse<T>  error(){
+        return new RestResponse<T> (null,null,-1);
+    }
+
+
+
+}
