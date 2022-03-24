e31b72bca429f6dcae0e74459e9b7ad174230915
cyx11 <2602388042@qq.com>
2022-03-24 10:09:14 +0800

    bugfix: add an annotation

--- Backend/src/main/java/com/xoj/backend/mapper/CompetitionMapper.java
+++ Backend/src/main/java/com/xoj/backend/mapper/CompetitionMapper.java
@@ -1,9 +1,11 @@
 package com.xoj.backend.mapper;
 
 import com.xoj.backend.entity.Competition;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author 1iin
  */
+@Mapper
 public interface CompetitionMapper extends BaseMapper<Competition> {
 }
