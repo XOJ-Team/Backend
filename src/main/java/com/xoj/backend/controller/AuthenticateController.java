6facd9001612f70870189915907dc1638f1bbca4
Sakai Kazuki <sakaikazuki@live.com>
2022-06-10 15:48:09 +0800

    feat: sesion authenticator

--- src/main/java/com/xoj/backend/controller/AuthenticateController.java
+++ src/main/java/com/xoj/backend/controller/AuthenticateController.java
@@ -1,6 +1,7 @@
 package com.xoj.backend.controller;
 
 import com.xoj.backend.base.RestResponse;
+import com.xoj.backend.notation.RequirePermission;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,12 @@ public class AuthenticateController {
     @RequestMapping(value = "/noAuthentication", method = RequestMethod.POST)
     public RestResponse<?> changeInfo2(Object obj) {
         return RestResponse.error("no authentication");
+    }
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    @RequirePermission
+    public RestResponse<?> authenticate(Object obj) {
+        return RestResponse.ok("Session ID is valid");
     }
 
 }
